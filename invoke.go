17f459b75fa3b408014dca492f0a0e4da096dff7
JulienVincendeau <julien.vincendeau@gmail.com>
2025-01-09 14:21:09 +0000

    no params

--- invoke.go
+++ invoke.go
@@ -6,37 +6,11 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"regexp"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	evt := r.URL.Query().Get("evt")
-	usecase := r.URL.Query().Get("usecase")
-	table := r.URL.Query().Get("table")
-	reload := r.URL.Query().Get("reload")
-	site := r.URL.Query().Get("site")
-	start_date := r.URL.Query().Get("start_date")
-	end_date := r.URL.Query().Get("end_date")
-	re := regexp.MustCompile(`^_?[a-zA-Z]+[a-zA-Z0-9\-_]*$`)
-
-	if !re.MatchString(evt) {
-		log.Fatalf("invalid evt criteria %q, evt required", evt)
-	}
-
-	if !re.MatchString(usecase) {
-		log.Fatalf("invalid usecase criteria %q, usecase required", usecase)
-	}
-
-	if !re.MatchString(table) {
-		log.Fatalf("invalid table criteria %q, table required", table)
-	}
-
-	if !re.MatchString(reload) {
-		log.Fatalf("invalid reload criteria %q, reload required", reload)
-	}
-
 	log.Print("Python Run: received a request")
-	cmd := exec.Command("/bin/sh", "script.sh", evt, usecase, table, reload, site, start_date, end_date)
+	cmd := exec.Command("/bin/sh", "script.sh")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
