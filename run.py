624f7a8f5dfeace7d3768be3e9e68b2ae4238cc1
JulienVincendeau <julien.vincendeau@gmail.com>
2025-01-09 15:11:55 +0000

    add cloudbuild

--- run.py
+++ run.py
@@ -1,14 +1,12 @@
+import datetime
+import os
+
 from google.cloud import bigquery
 
 client = bigquery.Client(project="sbx-mydigitalschool-2025")
 TABLE_ID = 'sbx-mydigitalschool-2025.test_grun.usa_1910_2013'
 
 def main():
-
-    QUERY = (f'TRUNCATE TABLE {TABLE_ID}  ')
-    query_job = client.query(QUERY)  # API request
-    rows = query_job.result()  # Waits for query to finish
-
     # Perform a query.
     QUERY = (
         'SELECT   `gender`,`name`,`number`,`state`,`year` '
@@ -31,5 +29,13 @@ def main():
             print("News rows added")
         else:
             print(f"!!Row {row.name} {row.state} on Error : {errors}")
+
+    if not os.path.exists('/tmp/exp'):
+        os.mknod('/tmp/exp')
+
+    with open("scr.txt", mode='w') as file:
+        file.write('Last recorded at %s.\n' % 
+                (datetime.datetime.now()))
+                
 
 main()
